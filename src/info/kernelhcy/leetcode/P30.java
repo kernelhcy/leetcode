fa3655413b4c406bf570489c38512ba28091faf4
hcy <huangcongyu2006@gmail.com>
2020-05-13 23:21:46 +0800

    add P30

---
+++ src/info/kernelhcy/leetcode/P30.java
@@ -0,0 +1,55 @@
+package info.kernelhcy.leetcode;
+
+import java.util.*;
+
+public class P30
+{
+    public static void main(String[] args)
+    {
+        System.out.println(new Solution().findSubstring("barfoothefoobarman", new String[]{"foo", "bar"}));
+        System.out.println(new Solution().findSubstring("barfoofoobarthefoobarman", new String[]{"foo", "bar", "the"}));
+        System.out.println(new Solution().findSubstring("wordgoodgoodgoodbestword", new String[]{"word","good","best","good"}));
+    }
+
+    public static class Solution
+    {
+        public List<Integer> findSubstring(String s, String[] words)
+        {
+            List<Integer> res = new LinkedList<>();
+            if (s.length() <= 0 || words.length <= 0) {
+                return res;
+            }
+
+            int wl = words[0].length();
+            int wsl = words[0].length() * words.length;
+
+            Map<String, Integer> wordsMap = new HashMap<>();
+            for (String word : words) {
+                wordsMap.put(word, wordsMap.getOrDefault(word, 0) + 1);
+            }
+
+            for (int i = 0; i < s.length(); ++i) {
+                Map<String, Integer> actual = new HashMap<>();
+                for (int j = i; j <= s.length() - wl; j += wl) {
+                    String tmp = s.substring(j, j + wl);
+                    if (wordsMap.containsKey(tmp)) {
+                        actual.put(tmp, actual.getOrDefault(tmp, 0) + 1);
+                        if (actual.get(tmp) > wordsMap.get(tmp)) {
+                            break;
+                        }
+
+                        if (j - i == wsl - wl) {
+                            res.add(i);
+                            break;
+                        }
+                    } else {
+                        break;
+                    }
+                }
+            }
+
+            return res;
+        }
+
+    }
+}
