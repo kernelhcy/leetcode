b1b31efa08e9d93e0dfffb613b4579339c027028
hcy <huangcongyu2006@gmail.com>
2020-05-17 14:11:26 +0800

    update P44

--- src/info/kernelhcy/leetcode/P44.java
+++ src/info/kernelhcy/leetcode/P44.java
@@ -1,16 +1,13 @@
 package info.kernelhcy.leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class P44
 {
     public static void main(String[] args)
     {
-        System.out.println(new Solution().isMatch2("aaa", "**a"));
-        System.out.println(new Solution().isMatch2("asd", "a*"));
-        System.out.println(new Solution().isMatch2("mississippi", "m??*ss*?i*pi"));
-        System.out.println(new Solution().isMatch2("adceb", "*a*b"));
+        System.out.println(new Solution().isMatch("aaa", "**a"));
+        System.out.println(new Solution().isMatch("asd", "a*"));
+        System.out.println(new Solution().isMatch("mississippi", "m??*ss*?i*pi"));
+        System.out.println(new Solution().isMatch("adceb", "*a*b"));
         System.out.println(new Solution().isMatch("babbbaabbaaaaabbababaaaabbbbbbbbbbabbaaaabbababbabaa", "*a*a*b*ab*a*bab"));
         System.out.println(new Solution().isMatch("babbbaabbaaaaabbababaaaabbbbbbbbbbabbaaaabbababbabaa", "**a****a**b***ab***a*bab"));
         System.out.println(new Solution().isMatch("abbabaaabbabbaababbabbbbbabbbabbbabaaaaababababbbabababaabbababaabbbbbbaaaabababbbaabbbbaabbbbababababbaabbaababaabbbababababbbbaaabbbbbabaaaabbababbbbaababaabbababbbbbababbbabaaaaaaaabbbbbaabaaababaaaabb",
@@ -21,41 +18,46 @@ public class P44
 
     public static class Solution
     {
+//        public boolean isMatch(String s, String p)
+//        {
+//            byte[] sa = s.getBytes();
+//            byte[] pa = p.getBytes();
+//
+//            int si = 0, lastSi = -1;
+//            int pi = 0, lastPi = -1;
+//            while (si < sa.length) {
+//                if (pi < pa.length && pa[pi] == '*') {
+//                    ++pi;
+//                    lastPi = pi;
+//                    lastSi = si;
+//                } else if (pi < pa.length && (pa[pi] == '?' || pa[pi] == sa[si])) {
+//                    ++si;
+//                    ++pi;
+//                } else if (lastSi >= 0){
+//                    ++lastSi;
+//                    si = lastSi;
+//                    pi = lastPi;
+//                } else {
+//                    return false;
+//                }
+//            }
+//
+//            while (pi < pa.length && pa[pi] == '*') ++pi;
+//            return pi >= pa.length;
+//        }
+
         public boolean isMatch(String s, String p)
         {
-            byte[] sa = s.getBytes();
-            byte[] pa = p.getBytes();
-
-            int si = 0, lastSi = -1;
-            int pi = 0, lastPi = -1;
-            while (si < sa.length) {
-                if (pi < pa.length && pa[pi] == '*') {
-                    ++pi;
-                    lastPi = pi;
-                    lastSi = si;
-                } else if (pi < pa.length && (pa[pi] == '?' || pa[pi] == sa[si])) {
-                    ++si;
-                    ++pi;
-                } else if (lastSi >= 0){
-                    ++lastSi;
-                    si = lastSi;
-                    pi = lastPi;
-                } else {
-                    return false;
-                }
-            }
-
-            while (pi < pa.length && pa[pi] == '*') ++pi;
-            return pi >= pa.length;
+            return isMatch2(s, p, new Condition());
         }
 
-        public boolean isMatch2(String s, String p)
+        private static class Condition
         {
-            String key = s + "|" + p;
-            if (res.containsKey(key)) {
-                return res.get(key);
-            }
+            boolean meetStar;
+        }
 
+        public boolean isMatch2(String s, String p, Condition con)
+        {
             if (p.equals("*")) return true;
             if (s.length() <= 0 && p.length() <= 0) return true;
             if (s.length() > 0 && p.length() <= 0) return false;
@@ -63,25 +65,25 @@ public class P44
 
             int i = 0, j = 0;
             for (; i < s.length(); ++i, ++j) {
-                if (j >= p.length()) {
-                    res.put(key, false);
-                    return false;
-                }
+                if (j >= p.length()) return false;
 
                 if (p.charAt(j) == '*') {
+                    con.meetStar = true;
                     while (j < p.length() - 1 && p.charAt(j + 1) == '*') ++j;
 
                     String np = p.substring(j + 1);
                     for (int k = i; k <= s.length(); ++k) {
-                        if (isMatch2(s.substring(k), np)) {
-                            res.put(key, true);
-                            return true;
-                        }
+                        Condition c = new Condition();
+                        c.meetStar = false;
+                        boolean re = isMatch2(s.substring(k), np, c);
+                        if (re) return true;
+                        // meeting a new *
+                        // do not eating more chars
+                        if (c.meetStar) return re;
                     }
                 } else if (p.charAt(j) == '?' || p.charAt(j) == s.charAt(i)) {
                     continue;
                 } else {
-                    res.put(key, false);
                     return false;
                 }
             }
@@ -89,7 +91,5 @@ public class P44
             while (j < p.length() && p.charAt(j) == '*') ++j;
             return j >= p.length();
         }
-
-        private Map<String, Boolean> res = new HashMap<>();
     }
 }
