a315fced01629859b3481ed470f5918dfb398c73
hcy <huangcongyu2006@gmail.com>
2020-06-04 23:46:40 +0800

    add P91

---
+++ src/info/kernelhcy/leetcode/P91.java
@@ -0,0 +1,38 @@
+package info.kernelhcy.leetcode;
+
+public class P91
+{
+    public static void main(String[] args)
+    {
+        System.out.println(new Solution().numDecodings("0"));
+        System.out.println(new Solution().numDecodings("00"));
+        System.out.println(new Solution().numDecodings("206"));
+        System.out.println(new Solution().numDecodings("226"));
+        System.out.println(new Solution().numDecodings("236"));
+        System.out.println(new Solution().numDecodings("22236"));
+        System.out.println(new Solution().numDecodings("230"));
+    }
+
+    public static class Solution
+    {
+        public int numDecodings(String s)
+        {
+            int n = s.length();
+
+            if(n == 0 || s.charAt(0) == '0') {
+                return 0;
+            }
+            int[] dp = new int[n+1];
+            dp[0] = 1;
+            dp[1] = 1;
+            for(int i = 2; i <= n; i++) {
+                dp[i] = 0;
+                if(s.charAt(i-1) > '0') dp[i] = dp[i-1];
+                if(s.charAt(i-2) == '1' || (s.charAt(i - 2) == '2' && s.charAt(i - 1) - '0' < 7)) dp[i] += dp[i-2];
+            }
+            return dp[n];
+        }
+
+
+    }
+}
